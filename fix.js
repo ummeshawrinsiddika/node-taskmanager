14d6cfb9ed59d7fcc607162225835d7520d19501
ummeshawrinsiddika <_email@gmail.com>
2026-04-19 01:31:37 +0600

    remove node_modules and fix.js

--- fix.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.connect(process.env.DB_URL).then(async () => {
-  await mongoose.connection.collection("users").deleteMany({});
-  console.log("deleted");
-  process.exit();
-});
